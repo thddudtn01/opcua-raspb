fc548ad110ee35f5b562c060f84f5363ebde2b0e
youngsoo <thddudtn01@gmail.com>
2020-11-21 17:14:32 +0900

    update traffic.py

--- traffic.py
+++ traffic.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 
 memory = sysv_ipc.SharedMemory( 777, flags=01000, size=10 ,mode=0600)
-memory.write("1")
+memory.write("3")
 
 GPIO.setmode(GPIO.BCM)
 
@@ -12,19 +12,18 @@ print "Setup LED pins as outputs"
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
-
 try:
     while True:
         light = memory.read()
-        if(light == '1'):
+        if(light[0] == "1"):
             GPIO.output(23, True)
             GPIO.output(24, False)
             GPIO.output(25, False)
-        if(light == '2'):
+        if(light[0] == '2'):
             GPIO.output(23, False)
             GPIO.output(24, True)
             GPIO.output(25, False)
-        if(light == '3'):
+        if(light[0] == '3'):
             GPIO.output(23, False)
             GPIO.output(24, False)
             GPIO.output(25, True)
