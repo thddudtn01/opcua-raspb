dcf5d99fa06cae4f667537958426460b11959d9e
thddudtn01 <54704254+thddudtn01@users.noreply.github.com>
2020-11-21 16:21:21 +0900

    Update m3.py

--- m3.py
+++ m3.py
@@ -7,17 +7,26 @@ print "Setup LED pins as outputs"
 
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(23, False)
+        GPIO.output(23, True)
         GPIO.output(24, False)
         GPIO.output(25, False)
-        time.sleep(1)
-        GPIO.output(23, True)
+        time.sleep(0.5)
+        GPIO.output(23, False)
         GPIO.output(24, True)
+        GPIO.output(25, False)
+        time.sleep(0.5)
+        GPIO.output(23, False)
+        GPIO.output(24, False)
         GPIO.output(25, True)
-        time.sleep(1)
+        time.sleep(0.5)
+        GPIO.output(23, False)
+        GPIO.output(24, True)
+        GPIO.output(25, False)
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
